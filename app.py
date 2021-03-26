936588cfc634a985653e5c2484ce3e632da9999a
Sakshi Saxena <sakshisaxena0808@gmail.com>
2021-03-26 12:59:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask , render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html', name=request.args.get("name"))
+
+if __name__ == "__main__":
+     app.run(debug=True, port=8000) 
