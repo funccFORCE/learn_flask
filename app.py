b9e06a4d28f6fcc9e408cdd814f987ef9e21e40b
Sakshi Saxena <sakshisaxena0808@gmail.com>
2021-03-28 13:33:19 +0530

    added SQLAlchemy database URI

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 from flask import Flask , render_template, request
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///funccforce.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Funccforce(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    coll = db.Column(db.String(200), nullable = False)
+    dept = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__ (self) -> str:
+        return f"{self.sno} - {self.name}"
+  
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method=="POST":
+        name = request.form['name']
+        coll = request.form['coll']
+        dept = request.form['dept']
+        funccforce = Funccforce(name=name, coll=coll, dept=dept)
+        db.session.add(funccforce)
+        db.session.commit()
+    allFunccforce=Funccforce.query.all()
+    return render_template('index.html', allFunccforce=allFunccforce)
 
 @app.route('/info')
 def info():
-    return render_template('info.html', name=request.args.get("name"))
-
-
+    allFunccforce = Funccforce.query.all()
+    print(allFunccforce)
+    return 'info page' 
+ 
 
 if __name__ == "__main__":
      app.run(debug=True, port=8000) 
